c92038b274a5ea3c76404478d65091f576bf7a80
guozant <guozan_t@qq.com>
2018-05-17 14:38:59 +0800

    test1

--- html/help/js/Util.js
+++ html/help/js/Util.js
@@ -713,7 +713,7 @@ ctx = Util.localStorage.get("ctx");
 
 
 // 点击安卓系统原生返回按钮调用backEvent函数，判断返回上一页
-window.backEvent = function () {
+/*window.backEvent = function () {
     window.location.href = 'http://www.baidu.com'
     var urlVal = window.location.href
     // 如果当前页面不是首页
@@ -722,7 +722,7 @@ window.backEvent = function () {
     } else {
         window.location.href = 'feedback.html'
     }
-}
+}*/
 
     /*if (hashVal !== '#/elecMoto' && hashVal !== '#/watch') {
         // 如果url里面的哈希值参数有“ #/elecMoto ”说明当前页面打开的是电动车页面
